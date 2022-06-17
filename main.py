559ad3fa39262a110018f5fffae08293764f6470
Chr0mat1que <tommysho123@gmailc.om>
2022-06-17 19:02:24 -0400

    test

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from tkinter import Text
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height=700, width=700, bg='#ffffcc')
+canvas = tk.Canvas(root, height=700, width=700, bg='#ffffcc') #this is kinda like the border type thingy. color code is baige
 canvas.pack()
 
 frame = tk.Frame(root, bg="White")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1) #indentationto make the broder centered
 
-Start = tk.Button(frame, text="Start", padx=10, pady=10)
+Start = tk.Button(frame, text="Start", padx=10, pady=10) # start button :D
 
 
 canvas.create_text(frame, 350, 75, text="Cookie Cracker", fill="black", font=('Helvetica 15 bold'))
@@ -19,8 +19,8 @@ root.mainloop()
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, tomiwa')  # Press Ctrl+F8 to toggle the breakpoint.
+        #Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, tomiwa')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.

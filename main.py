604d26058eb8ae1f61ed821d36de69a3c88faac8
isometrc <isometrc@protonmail.com>
2022-06-17 20:19:44 -0400

    Changed main.py to barebones UI

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
-import tkinter as tk
-from tkinter import Text
+from tkinter import *
+from tkinter import ttk
 
+root = Tk()
+frame = ttk.Frame(root, padding=10)
+frame.grid()
 
-root = tk.Tk()
+ttk.Label(frame, text="test text").grid(column=0, row=0)
+ttk.Label(frame, text="By Tomiwa Shobowale and Oghenetega Gbejewoh").grid(column=0, row=1);
 
-canvas = tk.Canvas(root, height=700, width=700, bg='#ffffcc') #this is kinda like the border type thingy. color code is baige
-canvas.pack()
-
-frame = tk.Frame(root, bg="White")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1) #indentationto make the broder centered
-
-Start = tk.Button(frame, text="Start", padx=10,
-                  pady=10, fg="blackBu", bg="#ffffcc") # start button :D
-Start.pack()
-
-#canvas.create_text(frame, 350, 75, text="Cookie Cracker", fill="black", font=('Helvetica 15 bold'))
-#canvas.pack()
 root.mainloop()
-
-
-#def print_hi(name):
-        #Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, tomiwa')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

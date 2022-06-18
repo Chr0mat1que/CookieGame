745ea65f4f2b193fd2977c74c72d97bd82f76350
Chr0mat1que <tommysho123@gmailc.om>
2022-06-18 13:27:45 -0400

    BUuton and updated ui

--- scenes.py
+++ scenes.py
@@ -13,6 +13,13 @@ def add_scene(tag, description, a, a_next, b, b_next, c, c_next):
     scenes.append(scene_info)
 
 def init_scenes(): # Add each possible scene here, one by one. Yes this sucks. No i can't think of a better way.
-    add_scene("0", "Hello World", "a", "0a", "b", "0b", "c", "0c")
+    add_scene("0",
+              "You walk into the house,you have been hired to find our who broke into the family house,to the second you walk in you notice all everyone screaming at each other, you don't like getting involed.You ask where the owners of the house are, they ignore you. Then the littlest one (couldn't be above 12) comes up to you and says 'are you blind? they obviously aren't here.' You get angry but dont show it, your their private detective after all, what do you say next?",
+              "Do you know where they went?",
+              "0a",
+              "How old are you kid?",
+              "0b",
+              "Then can YOU take me to the crime scene.",
+              "0c")
 
 init_scenes()

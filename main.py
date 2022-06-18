23cfba9e453f9fce828c057670050d265bebaf91
Chr0mat1que <tommysho123@gmailc.om>
2022-06-17 20:56:53 -0400

    changes

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from tkinter import ttk
 root = Tk()
 frame = ttk.Frame(root, padding=10)
 frame.grid()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
 
-ttk.Label(frame, text="test text").grid(column=0, row=0)
-ttk.Label(frame, text="By Tomiwa Shobowale and Oghenetega Gbejewoh").grid(column=0, row=1);
+ttk.Label(frame, text="Cookie Game").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Label(frame, text="By Oluwatomiwa Shobowale and Oghenetega Gbejewoh").grid(column=0, row=1);
 
 root.mainloop()

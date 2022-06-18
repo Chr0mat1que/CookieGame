332b1cdc055625f589a29b0102dddf14c97e5f01
isometrc <isometrc@protonmail.com>
2022-06-17 20:53:50 -0400

    Added start page layout and text

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-frame = ttk.Frame(root, padding=10)
+root.geometry("800x600")
+frame = ttk.Frame(root)
 frame.grid()
 
-ttk.Label(frame, text="test text").grid(column=0, row=0)
-ttk.Label(frame, text="By Tomiwa Shobowale and Oghenetega Gbejewoh").grid(column=0, row=1);
+desc = ttk.Label(frame, text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", wraplength=800, justify="center") # This is the description text, stating what the game is about and other important information.
+desc.grid(column=0, columnspan=2, row=0)
+
+start_btn = ttk.Button(frame, text="Start").grid(column=0, row=1)
+exit_btn = ttk.Button(frame, text="Exit").grid(column=1, row=1)
+
+ttk.Label(frame, text="By Tomiwa Shobowale and Oghenetega Gbejewoh").grid(column=0, columnspan=2, row=2); # Credits
 
 root.mainloop()
